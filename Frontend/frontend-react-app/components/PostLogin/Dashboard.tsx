b74110a580ee7ba82dce80e69a85c87b460d7d12
Dit Dejphachon <Ditdejphachon@gmail.com>
2022-04-27 18:32:50 +0700

    edited the dashboard #8

--- Frontend/frontend-react-app/components/PostLogin/Dashboard.tsx
+++ Frontend/frontend-react-app/components/PostLogin/Dashboard.tsx
@@ -11,13 +11,11 @@ const Dashboard = () => {
 
   const getInfo = async () => {
     const _gameInfo = await gameService.getAllGames();
-    console.log(_gameInfo);
     setGameInfo(_gameInfo);
-    console.log(gameInfo);
-    // const _categoryInfo = await gameCategoryService.getAllGameCategories();
-    // setCategoryInfo(_categoryInfo);
+    const _categoryInfo = await gameCategoryService.getAllGameCategories();
+    setCategoryInfo(_categoryInfo);
 
-    // replace id category with string category
+    //replace id category with string category
     // gameInfo.forEach((game) => {
     //   gameCategoryInfo.forEach((cat: CategoryInfo) => {
     //     if (game.category === cat.id) {
@@ -33,47 +31,44 @@ const Dashboard = () => {
   }, []);
   return (
     <div>
-      <div
-        className="grid grid-cols-4 gap-12
-          sm:grid-cols-5 
-          lg:grid-cols-6
-          xl:grid-cols-8 
-          "
-      >
-        {/* {gameCategoryInfo.map((info) => {
-          switch (info.id) {
-            case 1:
-              gameInfo.map((info) => {});
-              return;
-              break;
-            case 2:
-              break;
-            case 3:
-              break;
-            case 4:
-              break;
-            case 5:
-              break;
-            case 6:
-              break;
-            case 7:
-              break;
-            default:
-              break;
-          }
-        })} */}
-        {gameInfo.map((info) => {
-          return (
-            <Card
-              thumbnail={info.thumbnail}
-              name={info.name}
-              likePercentage={info.likePercentage}
-              viewCount={info.viewCount}
-              category={info.category}
-            ></Card>
-          );
-        })}
-      </div>
+      {[
+        'Fighting & Battle',
+        'Role-Playing',
+        'Action',
+        'Adventure',
+        'Collector Simulator',
+        'Tycoon',
+        'Tycoon & Strategy',
+      ].map((context) => {
+        return (
+          <div key={context}>
+            <h4 className="mt-8 text-3xl font-normal leading-normal  mb-2 dark:text-gray-900">
+              {context}
+            </h4>
+            <div
+              className="grid grid-cols-4 gap-12
+                    sm:grid-cols-5 
+                    lg:grid-cols-6
+                    xl:grid-cols-8 
+                    "
+            >
+              {gameInfo.map((info, index) => {
+                if (index < 5)
+                  // ! hardcoded
+                  return (
+                    <Card
+                      thumbnail={info.thumbnail}
+                      name={info.name}
+                      likePercentage={info.likePercentage}
+                      viewCount={info.viewCount}
+                      category={info.category}
+                    ></Card>
+                  );
+              })}
+            </div>
+          </div>
+        );
+      })}
     </div>
   );
 };
