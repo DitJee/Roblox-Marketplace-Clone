5b013d5ad2513bd0801d7f472967bfe4ca2e8c1b
Dit Dejphachon <Ditdejphachon@gmail.com>
2022-05-17 18:35:03 +0700

    #20 added `Add Creator` in Royalties splitter

--- Frontend/market-place-app/components/PostLogin/Create/Steps/RoyaltiesStep/RoyaltiesSpiltter.tsx
+++ Frontend/market-place-app/components/PostLogin/Create/Steps/RoyaltiesStep/RoyaltiesSpiltter.tsx
@@ -12,6 +12,15 @@ import { Formik, Field, Form, ErrorMessage, FieldArray } from "formik";
 import * as Yup from "yup";
 import { PlusCircleIcon, MinusCircleIcon } from "@heroicons/react/solid";
 
+const successfulClassName: { bar: string; icon: string } = {
+  bar: "bg-teal-100 border-t-4 border-teal-500 rounded-b text-teal-900 px-4 py-3 shadow-md",
+  icon: "fill-current h-6 w-6 text-teal-500 mr-4",
+};
+const unSuccessfulClassName: { bar: string; icon: string } = {
+  bar: "bg-red-100 border-t-4 border-red-500 rounded-b text-red-900 px-4 py-3 shadow-md",
+  icon: "fill-current h-6 w-6 text-red-500 mr-4",
+};
+
 const validationSchema = () => {
   return Yup.object().shape({
     royalties: Yup.array().of(
@@ -36,8 +45,13 @@ const RoyaltiesSpiltter = (props: {
   royalties: Array<Royalty>;
   setRoyalties: Function;
   isShowErrors?: boolean;
+  attributes: IMetadataExtension;
+  setAttributes: (attr: IMetadataExtension) => void;
 }) => {
   const { publicKey, connected } = useWallet();
+
+  const [bSuccessful, setbSuccessful] = useState(false);
+  const [message, setMessage] = useState("");
 
   const initialValues: {
     royalties: { creatorWalletAddress: string; splitPercentage: number }[];
@@ -48,6 +62,23 @@ const RoyaltiesSpiltter = (props: {
         splitPercentage: 100,
       },
     ],
+  };
+
+  const onRoyaltyPercentageChange = (e) => {
+    let _percentage: number = 0;
+
+    if (e.target.value >= 100) {
+      _percentage = 100;
+    } else if (e.target.value <= 0) {
+      _percentage = 0;
+    } else {
+      _percentage = e.target.value;
+    }
+
+    props.setAttributes({
+      ...props.attributes,
+      seller_fee_basis_points: _percentage * 100,
+    });
   };
 
   return (
@@ -64,10 +95,24 @@ const RoyaltiesSpiltter = (props: {
             <FieldArray name="royalties">
               {({ insert, remove, push }) => (
                 <div>
+                  <label className="block  text-2xl font-semibold text-gray-900 ">
+                    {"Royalty Percentage"}
+                  </label>
+                  <h4 className="">
+                    This is how much of each secondary sale will be paid out to
+                    the creators.
+                  </h4>
+                  <input
+                    className="inputRow"
+                    type={"number"}
+                    placeholder={"100"}
+                    onChange={onRoyaltyPercentageChange}
+                    required
+                  />
                   <div className="flex items-center justify-start mb-5 ">
                     <button
                       type="button"
-                      className=""
+                      className="secondary"
                       onClick={() =>
                         push({ creatorWalletAddress: "", splitPercentage: "" })
                       }
@@ -135,10 +180,83 @@ const RoyaltiesSpiltter = (props: {
                 </div>
               )}
             </FieldArray>
+            <div className="form-group flex justify-center">
+              <button
+                type="submit"
+                className="text-purple-800 bg-gray-100 px-7 py-2 shadow-md    rounded-full font-bold my-2 hover:shadow-xl active:scale-90 transition duration-150"
+                disabled={bSuccessful}
+              >
+                <div className="flex items-center space-x-2">
+                  <span>Continue to Royalties</span>
+                  {bSuccessful && (
+                    <div
+                      className="flex spinner-border animate-spin  w-8 h-8 border-4 rounded-full"
+                      role="status"
+                    >
+                      <span className="visually-hidden"></span>
+                    </div>
+                  )}
+                </div>
+              </button>
+            </div>
+            {message && (
+              <div
+                className={
+                  bSuccessful
+                    ? successfulClassName.bar
+                    : unSuccessfulClassName.bar
+                }
+                role="alert"
+              >
+                <div className="flex">
+                  <div className="py-1">
+                    <svg
+                      className={
+                        bSuccessful
+                          ? successfulClassName.icon
+                          : unSuccessfulClassName.icon
+                      }
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 20 20"
+                    >
+                      <path d="M2.93 17.07A10 10 0 1 1 17.07 2.93 10 10 0 0 1 2.93 17.07zm12.73-1.41A8 8 0 1 0 4.34 4.34a8 8 0 0 0 11.32 11.32zM9 11V9h2v6H9v-4zm0-6h2v2H9V5z" />
+                    </svg>
+                  </div>
+                  <div>
+                    <div className="font-normal">{}</div>
+                    <div className="text-sm py-1 items-center">{message}</div>
+                  </div>
+                </div>
+              </div>
+            )}
           </Form>
         )}
       </Formik>
-      <button type="submit">asdd</button>
+      {props.isShowErrors && (
+        <div className={unSuccessfulClassName.bar} role="alert">
+          <div className="flex">
+            <div className="py-1">
+              <svg
+                className={
+                  bSuccessful
+                    ? successfulClassName.icon
+                    : unSuccessfulClassName.icon
+                }
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 20 20"
+              >
+                <path d="M2.93 17.07A10 10 0 1 1 17.07 2.93 10 10 0 0 1 2.93 17.07zm12.73-1.41A8 8 0 1 0 4.34 4.34a8 8 0 0 0 11.32 11.32zM9 11V9h2v6H9v-4zm0-6h2v2H9V5z" />
+              </svg>
+            </div>
+            <div>
+              <div className="font-normal">{}</div>
+              <div className="text-sm py-1 items-center">
+                The split percentage for this creator cannot be 0%.
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
--- Frontend/market-place-app/components/PostLogin/Create/Steps/RoyaltiesStep/RoyaltiesStep.tsx
+++ Frontend/market-place-app/components/PostLogin/Create/Steps/RoyaltiesStep/RoyaltiesStep.tsx
@@ -11,6 +11,7 @@ import { shortenAddress } from "../../../../../utils/string";
 import { Formik, Field, Form, ErrorMessage } from "formik";
 import * as Yup from "yup";
 import RoyaltiesSpiltter from "./RoyaltiesSpiltter";
+import { v4 as uuidv4 } from "uuid";
 
 const validationSchema = () => {
   return Yup.object().shape({
@@ -18,16 +19,21 @@ const validationSchema = () => {
       .min(0, "The percentage must be more than 0%")
       .max(100, "The percentage must be less than 100%")
       .required("Royalty percentage is required"),
+    royalties: Yup.array().of(
+      Yup.object().shape({
+        creatorWalletAddress: Yup.string().test(
+          "len",
+          "About section must be between 0 and 45 characters.",
+          (val: any) =>
+            val && val.toString().length >= 0 && val.toString().length <= 45
+        ),
+        splitPercentage: Yup.number()
+          .min(0, "The percentage must be more than 0%")
+          .max(100, "The percentage must be less than 100%")
+          .required("Royalty split percentage is required"),
+      })
+    ),
   });
-};
-
-const successfulClassName: { bar: string; icon: string } = {
-  bar: "bg-teal-100 border-t-4 border-teal-500 rounded-b text-teal-900 px-4 py-3 shadow-md",
-  icon: "fill-current h-6 w-6 text-teal-500 mr-4",
-};
-const unSuccessfulClassName: { bar: string; icon: string } = {
-  bar: "bg-red-100 border-t-4 border-red-500 rounded-b text-red-900 px-4 py-3 shadow-md",
-  icon: "fill-current h-6 w-6 text-red-500 mr-4",
 };
 
 const RoyaltiesStep = (props: {
@@ -35,9 +41,6 @@ const RoyaltiesStep = (props: {
   setAttributes: (attr: IMetadataExtension) => void;
   confirm: () => void;
 }) => {
-  const [bSuccessful, setbSuccessful] = useState(false);
-  const [message, setMessage] = useState("");
-
   // const file = props.attributes.image;
   const { publicKey, connected } = useWallet();
   const [creators, setCreators] = useState<Array<UserValue>>([]);
@@ -141,146 +144,32 @@ const RoyaltiesStep = (props: {
         after its initial sale.
       </h3>
 
-      <Formik
-        initialValues={initialValues}
-        validationSchema={validationSchema}
-        onSubmit={handleGoToReview}
-      >
-        <Form>
-          {[
-            {
-              context: "Royalty Percentage",
-              name: "royaltyPercentage",
-              type: "number",
-              placeholder: "Between 0 and 100",
-            },
-          ].map((element, index) => {
-            return (
-              <div key={element.context} className="form-group">
-                <label
-                  htmlFor={element.name}
-                  className="block  text-2xl font-semibold text-gray-900 "
-                >
-                  {element.context}
-                </label>
-                <h4 className="">
-                  This is how much of each secondary sale will be paid out to
-                  the creators.
-                </h4>
-                <Field
-                  name={element.name}
-                  type={element.type}
-                  placeholder={element.placeholder}
-                  className="inputRow"
-                />
-                <ErrorMessage
-                  name={element.name}
-                  component="div"
-                  className="mt-5 mb-5 flex items-center border dark:border-red-800 bg-gray-700  text-white text-sm font-bold px-4 py-3"
-                />
-                {/* 
+      <div className="form-group">
+        {/* 
                   RoyaltiesSpiltter 
                 */}
-                {[...fixedCreators, ...creators].length > 0 && (
-                  <div>
-                    <label className="action-field" style={{ width: "100%" }}>
-                      <h1 className="text-2xl font-bold leading-normal text-black">
-                        Creators Split
-                      </h1>
-                      <p>
-                        This is how much of the proceeds from the initial sale
-                        and any royalties will be split out amongst the
-                        creators.
-                      </p>
-                      <RoyaltiesSpiltter
-                        creators={[...fixedCreators, ...creators]}
-                        royalties={royalties}
-                        setRoyalties={setRoyalties}
-                        isShowErrors={isShowErrors}
-                      />
-                    </label>
-                  </div>
-                )}
-              </div>
-            );
-          })}
-
-          <div className="form-group flex justify-center">
-            <button
-              type="submit"
-              className="text-purple-800 bg-gray-100 px-7 py-2 shadow-md    rounded-full font-bold my-2 hover:shadow-xl active:scale-90 transition duration-150"
-              disabled={bSuccessful}
-            >
-              <div className="flex items-center space-x-2">
-                <span>Continue to Royalties</span>
-                {bSuccessful && (
-                  <div
-                    className="flex spinner-border animate-spin  w-8 h-8 border-4 rounded-full"
-                    role="status"
-                  >
-                    <span className="visually-hidden"></span>
-                  </div>
-                )}
-              </div>
-            </button>
+        {[...fixedCreators, ...creators].length > 0 && (
+          <div>
+            <label className="action-field" style={{ width: "100%" }}>
+              <h1 className="text-2xl font-bold leading-normal text-black">
+                Creators Split
+              </h1>
+              <p>
+                This is how much of the proceeds from the initial sale and any
+                royalties will be split out amongst the creators.
+              </p>
+              <RoyaltiesSpiltter
+                creators={[...fixedCreators, ...creators]}
+                royalties={royalties}
+                setRoyalties={setRoyalties}
+                isShowErrors={isShowErrors}
+                attributes={props.attributes}
+                setAttributes={props.setAttributes}
+              />
+            </label>
           </div>
-          {message && (
-            <div
-              className={
-                bSuccessful
-                  ? successfulClassName.bar
-                  : unSuccessfulClassName.bar
-              }
-              role="alert"
-            >
-              <div className="flex">
-                <div className="py-1">
-                  <svg
-                    className={
-                      bSuccessful
-                        ? successfulClassName.icon
-                        : unSuccessfulClassName.icon
-                    }
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 20 20"
-                  >
-                    <path d="M2.93 17.07A10 10 0 1 1 17.07 2.93 10 10 0 0 1 2.93 17.07zm12.73-1.41A8 8 0 1 0 4.34 4.34a8 8 0 0 0 11.32 11.32zM9 11V9h2v6H9v-4zm0-6h2v2H9V5z" />
-                  </svg>
-                </div>
-                <div>
-                  <div className="font-normal">{}</div>
-                  <div className="text-sm py-1 items-center">{message}</div>
-                </div>
-              </div>
-            </div>
-          )}
-        </Form>
-      </Formik>
-      {isShowErrors && (
-        <div className={unSuccessfulClassName.bar} role="alert">
-          <div className="flex">
-            <div className="py-1">
-              <svg
-                className={
-                  bSuccessful
-                    ? successfulClassName.icon
-                    : unSuccessfulClassName.icon
-                }
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 20 20"
-              >
-                <path d="M2.93 17.07A10 10 0 1 1 17.07 2.93 10 10 0 0 1 2.93 17.07zm12.73-1.41A8 8 0 1 0 4.34 4.34a8 8 0 0 0 11.32 11.32zM9 11V9h2v6H9v-4zm0-6h2v2H9V5z" />
-              </svg>
-            </div>
-            <div>
-              <div className="font-normal">{}</div>
-              <div className="text-sm py-1 items-center">
-                The split percentage for this creator cannot be 0%.
-              </div>
-            </div>
-          </div>
-        </div>
-      )}
+        )}
+      </div>
     </div>
   );
 };
